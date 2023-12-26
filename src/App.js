ce7861e397b99c455c01d4a88c82077fc48d08d2
Vivek Kulhar <vivek.kulhar23@gmail.com>
2023-12-26 14:53:30 +0530

    Nav bar design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import NavBar from './components/NavBar';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex border space-x-8 items-center pl-3 py-4'>
+      <h3 className='text-blue-400'>Movies</h3>
+      <h3 className='text-blue-400'>WatchList</h3>
     </div>
   );
 }
