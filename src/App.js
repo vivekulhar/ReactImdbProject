26427cd306f3f595166b05e223ee133dba2f7686
Vivek Kulhar <vivek.kulhar23@gmail.com>
2023-12-26 15:58:53 +0530

    Nav bar image

--- src/App.js
+++ src/App.js
@@ -3,10 +3,7 @@ import './App.css';
 import NavBar from './components/NavBar';
 function App() {
   return (
-    <div className='flex border space-x-8 items-center pl-3 py-4'>
-      <h3 className='text-blue-400'>Movies</h3>
-      <h3 className='text-blue-400'>WatchList</h3>
-    </div>
+    <NavBar />
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import MovieLogo from '../movielogo.png'
 
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <div className='flex border space-x-8 items-center pl-3 py-4'>
+      <img src={MovieLogo} className='w-[50px]'/>
+      <h3 className='text-blue-400'>Movies</h3>
+      <h3 className='text-blue-400'>WatchList</h3>
+    </div>
   )
 }
 
