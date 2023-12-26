adf8a96868020876c2c08bc5a1ff0c099ba123c4
Vivek Kulhar <vivek.kulhar23@gmail.com>
2023-12-27 05:23:41 +0530

    watchlist

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,13 +1,15 @@
 import React, { useEffect , useState} from "react";
 import axios from "axios";
 import Pagination from "./Pagination";
+import WatchList from "./WatchList";
 function Movies() {
 
     const [movies, setMovies] = useState([])
     const [pageNum, setPageNum] = useState(1)
     const [watchList, setWatchList] = useState([])
+    const [hovered, setHovered] = useState('')
 
-    // pagination methods
+    // pagination Handlers
     const onNext = () => {
         setPageNum(pageNum + 1)
     }
@@ -22,8 +24,19 @@ function Movies() {
         const newWatchList = [...watchList, id]
         setWatchList(newWatchList)
     }
+    const removeFromWatchList = (id) => {
+        const filteredWatchList = watchList.filter((item) => item !== id)
+        setWatchList(filteredWatchList)
+    }
 
-    console.log(watchList)
+    const showButton = (id) => {
+        setHovered(id)
+    }
+    const hideButton = (id) => {
+        setHovered('')
+    }
+
+    console.log(movies)
     useEffect(() => {
         // good practice to use an IIFE
         // IIFE = Immediately Invoked Function Expression
@@ -51,17 +64,24 @@ function Movies() {
         {movies.map((movie) => {
           return (
             <div
+                onMouseOver = {() => showButton(movie.id)}
+                onMouseLeave = {() => hideButton(movie.id)}
               key={movie.id}
               className="w-[200px] h-[35vh] bg-center bg-cover rounded-xl m-4 md:h[40vh] md:w[180px] hover:scale-110 duration-300 relative flex items-end"
               style={{
                 backgroundImage: `url(https://image.tmdb.org/t/p/original/t/p/w500/${movie.poster_path})`,
               }}
+
+
             >
-              {/* {} */}
-              <div
-                className="text-xl p-2 bg-gray-900 rounded-xl absolute right-2 top-2 opacity-70"
+              <div className="text-xl p-2 bg-gray-900 rounded-xl absolute right-2 top-2 opacity-70"
+              style={{display:hovered==movie.id?'block':'none'}}
               >
-                <div onClick={()=>addToWatchList(movie.id)}>🤩</div>
+                {watchList.includes(movie.id) == false ? (
+                  <div onClick={() => addToWatchList(movie.id)}>🤩</div>
+                ) : (
+                  <div onClick={()=>removeFromWatchList(movie.id)}>❌</div>
+                )}
               </div>
 
               <div className="text-white font-bold text-center w-full bg-gray-900 bg-opacity-50">
@@ -75,6 +95,7 @@ function Movies() {
       </div>
 
       <Pagination pageNum={pageNum} onNext={onNext} onPrev = {onPrev} />
+      
     </div>
   );
 }
