8bef0e351ecf1525f9a053d685df28c1c82083f4
Vivek Kulhar <vivek.kulhar23@gmail.com>
2023-12-26 17:05:22 +0530

    Link in React Router DOM

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <>
-      <NavBar />
       <BrowserRouter>
+        <NavBar />
+
         <Routes>
           <Route
             path="/"
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import MovieLogo from '../movielogo.png'
-
+import { Link } from 'react-router-dom'
 function NavBar() {
   return (
     <div className='flex border space-x-8 items-center pl-3 py-4'>
       <img src={MovieLogo} className='w-[50px]'/>
-      <h3 className='text-blue-400'>Movies</h3>
-      <h3 className='text-blue-400'>WatchList</h3>
+      <Link to='/' className='text-blue-400'>Movies</Link>
+      <Link to='/watchlist' className='text-blue-400'>WatchList</Link>
     </div>
   )
 }
 
-export default NavBar
+export default NavBar 
