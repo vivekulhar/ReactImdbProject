c3ffdce2f1ba499d42f638473d81190bc2a69ccb
Vivek Kulhar <vivek.kulhar23@gmail.com>
2023-12-31 13:15:22 +0530

    Watchlist Genres

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,6 +2,7 @@ import React, { useEffect , useState} from "react";
 import axios from "axios";
 import Pagination from "./Pagination";
 import WatchList from "./WatchList";
+import { json } from "react-router-dom";
 function Movies() {
 
     const [movies, setMovies] = useState([])
@@ -21,28 +22,34 @@ function Movies() {
     // WatchList Handlers
     const addToWatchList = (movie) => {
         // ... is the spread operator
-        const newWatchList = [...watchList, movie]
+        let newWatchList = JSON.parse(localStorage.getItem('imdb')) || []
+         newWatchList = [...newWatchList, movie]
         setWatchList(newWatchList)
         localStorage.setItem('imdb', JSON.stringify(newWatchList))
     }
     const removeFromWatchList = (movie) => {
-        const filteredWatchList = watchList.filter((item) => item.id !== movie.id)
+        let newWatchList = JSON.parse(localStorage.getItem('imdb')) || []
+        const filteredWatchList = newWatchList.filter((item) => item.id != movie.id)
         setWatchList(filteredWatchList)
-    }
+        localStorage.setItem('imdb', JSON.stringify(filteredWatchList))
+    };
 
     const showButton = (id) => {
         setHovered(id)
-    }
+    };
     const hideButton = (id) => {
         setHovered('')
-    }
+    };
 
-    console.log(movies)
+  
+    
     useEffect(() => {
         // good practice to use an IIFE
         // IIFE = Immediately Invoked Function Expression
         // use function inside useEffect
         (function(){
+            let moviesFromLocalStorage = JSON.parse(localStorage.getItem("imdb")) || [];
+            setWatchList(moviesFromLocalStorage);
             axios
               .get(
                 `https://api.themoviedb.org/3/trending/movie/day?api_key=6bf4ade6a0cbdd6c609693a468fb080f&page=${pageNum}   `
@@ -65,23 +72,33 @@ function Movies() {
         {movies.map((movie) => {
           return (
             <div
-                onMouseOver = {() => showButton(movie.id)}
-                onMouseLeave = {() => hideButton(movie.id)}
+              onMouseOver={() => showButton(movie.id)}
+              onMouseLeave={() => hideButton(movie.id)}
               key={movie.id}
               className="w-[200px] h-[35vh] bg-center bg-cover rounded-xl m-4 md:h[40vh] md:w[180px] hover:scale-110 duration-300 relative flex items-end"
               style={{
                 backgroundImage: `url(https://image.tmdb.org/t/p/original/t/p/w500/${movie.poster_path})`,
               }}
-
-
             >
-              <div className="text-xl p-2 bg-gray-900 rounded-xl absolute right-2 top-2 opacity-70"
-              style={{display:hovered==movie.id?'block':'none'}}
+              <div
+                className="text-xl p-2 bg-gray-900 rounded-xl absolute right-2 top-2 opacity-70"
+                style={{ display: hovered == movie.id ? "block" : "none" }}
               >
-                {watchList.includes(movie) == false ? (
+                {/* {watchList.includes(movie) == false ? (
                   <div onClick={() => addToWatchList(movie)}>🤩</div>
                 ) : (
                   <div onClick={()=>removeFromWatchList(movie)}>❌</div>
+                )} */}
+                {/* {!watchList.some((item) => item.id === movie.id) ? (
+                  <div onClick={() => addToWatchList(movie)}>🤩</div>
+                ) : (
+                  <div onClick={() => removeFromWatchList(movie)}>❌</div>
+                )} */}
+                {!watchList.filter((item) => item.id === movie.id).length >
+                0 ? (
+                  <div onClick={() => addToWatchList(movie)}>🤩</div>
+                ) : (
+                  <div onClick={() => removeFromWatchList(movie)}>❌</div>
                 )}
               </div>
 
